f1ff0c299508b35f70050d3a0da5d73905d8060e
Braian Staimer <you@example.com>
2020-07-26 00:56:03 -0600

    + initial commit

--- services/files/files.py
+++ services/files/files.py
@@ -11,21 +11,24 @@ def get_from_source(source):
     :param source: URL that will be use to get the file
     """
 
-    """Prepare the payload"""
-    _url = source
+    try:
+        """Prepare the payload"""
+        _url = source
 
-    """Fetch file from the URL"""
-    _file_req = requests.get(
-        _url
-    )
+        """Fetch file from the URL"""
+        _file_req = requests.get(
+            _url
+        )
 
-    """Check if the response is valid"""
-    if _file_req.status_code != 200:
-        """Return error if the response is invalid"""
-        return _file_req.json()
+        """Check if the response is valid"""
+        if _file_req.status_code != 200:
+            """Return error if the response is invalid"""
+            return _file_req.json()
 
-    """Transform the data"""
-    _file_data = _file_req.content
+        """Transform the data"""
+        _file_data = _file_req.content
 
-    """Return data"""
-    return success_response_service(data=_file_data)
+        """Return data"""
+        return success_response_service(data=_file_data)
+    except Exception as error:
+        return error_response_service(msg=str(error))
